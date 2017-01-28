bcdd8b5cb02ad3400c0e657dd097428db73d3c5d
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-01-28 12:46:35 +0100

    Add ACK in mailbox

--- README.md
+++ README.md
@@ -8,11 +8,20 @@ Some examples of last Akka distribution of lightbend
 
 * **Actors**
 
-    To run execution run [Runner](src/main/scala/Runner.scala)
-
     ![My image](src/main/resources/img/akkaActor.png)
     * [Master] (src/main/scala/actor/Master.scala)
     * [Worker] (src/main/scala/actor/Worker.scala)
     * [Listener] (src/main/scala/actor/Listener.scala)
+
+    To run execution run [Runner](src/main/scala/Runner.scala)
+
+* **Stream**
+
+    ![My image](src/main/resources/img/stream.png)
+    * [Source/Sink] (src/main/scala/actor/AkkaStream.scala)
+    * [Flow] (src/main/scala/actor/Flow.scala)
+    * [Listener] (src/main/scala/actor/Subscriber.scala)
+
+* [Agent] (src/main/scala/agents/Agents.scala)
 
 
--- src/main/scala/agents/Agents.scala
+++ src/main/scala/agents/Agents.scala
@@ -10,22 +10,28 @@ import scala.concurrent.Future
 
 /**
   * Agents are instance linked with a resource, and only this agent it´s able to mutate the resource
-  * The api of the agent allow us to update the resource sync and async
+  * The api of the agent allow us to update the resource in a sync and async fashion way
+  * Also when we decide to update the resource in async way, we can get a promise from the agent
+  * to determine when the task as been finish
   */
 object Agents extends App {
 
   implicit val context = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
+  val agent = Agent(5)
+
   runAgent()
 
   def runAgent() {
-    val agent = Agent(5)
     println(s"Read agent value:${agent.get()}")
+
     val update: Future[Int] = agent.alter(10)
     update.onComplete(value => println(s"Agent update value:${value.get}"))
+
     agent.send(value => value + 100)
     println(s"Async update not guaranteed:${agent.get()}")
+
     agent.future().onComplete(value => println(s"Wait for data updated:${value.get}"))
   }
 
