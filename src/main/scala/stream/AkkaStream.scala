ced80519a0ed2bafb0453a6bdab81f875b625ed4
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-01-28 23:54:11 +0100

    Add stream operators

--- src/main/scala/stream/AkkaStream.scala
+++ src/main/scala/stream/AkkaStream.scala
@@ -4,6 +4,9 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Sink, Source}
 
+import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContext, Future}
+
 /**
   * Created by pabloperezgarcia on 25/01/2017.
   *
@@ -23,16 +26,57 @@ object AkkaStream extends App {
   implicit val materializer = ActorMaterializer()
 
   sourceToSink()
+  tick()
+  repeat()
+  mapAsync()
 
   def sourceToSink(): Unit = {
     val sink = Sink.foreach(println)
 
     val source = Source(List("Akka", "Kafka", "Afakka", "Kakfta"))
+      .alsoTo(Sink.foreach(s => println(s"Input:$s")))
       .map(syncVal)
       .filter(_.contains("AKKA"))
-
     source to sink run()
+  }
 
+  /**
+    * Tick operator is like interval in Rx it will repeat the emittion of item with an initial delay
+    * and an internal delay
+    */
+  def tick(): Unit = {
+    Source.tick(0 seconds, 1 seconds, "Tick")
+      .map(value => value.toUpperCase)
+      .to(Sink.foreach(value => println(s"item emitted:$value")))
+      .run()
+  }
+
+  /**
+    * Create a `Source` that will continually emit the given element.
+    */
+  def repeat(): Unit = {
+    Source.repeat("Repeat")
+      .map(value => value.toUpperCase)
+      .to(Sink.foreach(value => println(s"item emitted:$value")))
+      .run()
+  }
+
+  /**
+    * Using mapAsync operator, we pass a function which return a Future, the number of parallel run futures will
+    * be determine by the argument passed to the operator
+    */
+  def mapAsync(): Unit = {
+    Source(0 to 10)
+      .mapAsync(2) { value =>
+        implicit val ec: ExecutionContext = ActorSystem().dispatcher
+        Future {
+          Thread.sleep(500)
+          println(s"Process in Thread:${Thread.currentThread().getName}")
+          value
+        }
+      }
+      .to(Sink.foreach(value => println(s"Item emitted:$value in Thread:${Thread.currentThread().getName}")))
+      .run()
   }
 
   def syncVal: String => String = {
