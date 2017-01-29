da27957e61d0f6f1aacd8af23e320c2e5fd1e594
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-01-29 19:28:41 +0100

    Add stream operators

--- src/main/scala/stream/Fow.scala
+++ src/main/scala/stream/Fow.scala
@@ -2,22 +2,33 @@ package stream
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import org.junit.Test
 
 /**
   * Created by pabloperezgarcia on 25/01/2017.
   */
-object Fow extends App {
+class Fow {
 
   implicit val context = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  run()
-
-  def run(): Unit = {
-//    val fetchStrings: Flow[String, String] =
-//      Flow.("A")
-//        .map(value => value.toUpperCase)
-
+  /**
+    * Flow are just pipeline where has 1 input item, and emitt 1 output item
+    * We just need to specify the type of item that we will received, and then
+    * we can use all operators that Akka stream api provide
+    */
+  @Test def mainFlow(): Unit = {
+    val increase = Flow[Int]
+      .map(value => value * 10)
+    val filterFlow = Flow[Int]
+      .filter(value => value > 50)
+      .take(2)
+    Source(0 to 10)
+      .via(increase)
+      .via(filterFlow)
+      .to(Sink.foreach(value => println(s"Item emitted:$value")))
+      .run()
   }
 
 }
