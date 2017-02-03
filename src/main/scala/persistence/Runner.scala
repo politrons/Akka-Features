b02918cbde89398e8cf4ef45fec17882f3436d20
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-02-03 09:56:43 +0100

    Akka persistence

--- src/main/scala/persistence/Runner.scala
+++ src/main/scala/persistence/Runner.scala
@@ -1,29 +1,24 @@
 package persistence
 
 import akka.actor.{ActorSystem, PoisonPill}
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
-import persistence.commands.AddItemCommand
-import persistence.response.AddItemResponse
 import persistence.actor.BasketActor
-import org.scalatest.WordSpecLike
-import org.scalatest.Matchers
-import org.scalatest.BeforeAndAfterAll
+import persistence.commands.AddItemCommand
 
 /**
   * Created by pabloperezgarcia on 03/02/2017.
   */
-class Runner extends TestKit(ActorSystem("MySpec")) with ImplicitSender
-  with WordSpecLike with Matchers with BeforeAndAfterAll {
-
-  override def afterAll: Unit = {
-    TestKit.shutdownActorSystem(system)
-  }
+object Runner extends App{
   val basketId = "sc-000001"
-  val shoppingCartActor = system.actorOf(BasketActor.props(basketId))
+
+  val system = ActorSystem("Politron-Chief")
+
+  system.mailboxes.deadLetterMailbox
+
+  val basketActor = system.actorOf(BasketActor.props(basketId))
 
   private val item = new Item(basketId)
-  shoppingCartActor ! AddItemCommand(item)
-  expectMsg(AddItemResponse(item))
+  basketActor ! AddItemCommand(item)
+//  AddItemResponse(item)
 
-  shoppingCartActor ! PoisonPill
+  basketActor ! PoisonPill
 }
