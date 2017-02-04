741c1eea4ea02b83fad0f6bb97854cb87bdd88a4
ZH11 <Pablo.Garcia@uk.tesco.com>
2017-02-04 18:49:23 +0100

    Akka persistence implementation

--- src/main/scala/stream/BackPressure.scala
+++ src/main/scala/stream/BackPressure.scala
@@ -3,7 +3,12 @@ package stream
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.{ActorMaterializer, OverflowStrategy}
+import org.junit.Test
 import org.reactivestreams.Subscriber
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 
 /**
   * Created by pabloperezgarcia on 28/01/2017.
@@ -11,15 +16,25 @@ import org.reactivestreams.Subscriber
   * Back pressure refers to pressure opposed to the desired flow of items in a pipe.
   * It is often caused by long process that normally makes the pipe start having more input items that can process out.
   */
-object BackPressure extends App {
+class BackPressure {
 
   implicit val system = ActorSystem()
   implicit val materializer = ActorMaterializer()
 
-  runSubscriber(buffer())
-  runSubscriber(bufferDropHead())
-  runSubscriber(bufferDropTail())
+  @Test
+  def runBuffer() = {
+    runSubscriber(buffer())
+  }
 
+  @Test
+  def runBufferDropHead() {
+    runSubscriber(bufferDropHead())
+  }
+
+  @Test
+  def runBufferDropTail() {
+    runSubscriber(bufferDropTail())
+  }
 
   def runSubscriber(function: () => Subscriber[Int]) {
     Source(1 to 10)
@@ -69,5 +84,17 @@ object BackPressure extends App {
       val sink = Sink.foreach(Console.println)
       source to sink run()
   }
+
+  /**
+    * GroupedWithin allow you group emission of items by a specific number of items or by a window time
+    */
+  @Test def groupedWithin(): Unit = {
+    Await.ready(Source(0 to 10)
+      .map(_.toString)
+      .groupedWithin(3, 5 millisecond)
+      .runForeach(list => println(s"List:$list")), 5 seconds)
+  }
+
+
 }
 
