e98b38577bb1d153c4f957332d46a61cff81c34f
politrons <politrons@gmail.com>
2019-09-15 12:44:43 +0200

    meetup example

--- AkkaStreamHttp/src/main/scala/stream/HttpAkkaStream.scala
+++ AkkaStreamHttp/src/main/scala/stream/HttpAkkaStream.scala
@@ -1,53 +1,70 @@
 package stream
 
 import akka.actor.ActorSystem
-import akka.event.Logging
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.{ActorMaterializer, Attributes}
+import akka.stream.ActorMaterializer
 import spray.json.DefaultJsonProtocol._
 import spray.json.RootJsonFormat
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
-object Main extends App {
-  implicit val system: ActorSystem = ActorSystem("Main")
+object HttpAkkaStream extends App {
+
+  implicit val system: ActorSystem = ActorSystem("AkkaStreamHttp")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContext = system.dispatcher
 
+  private val port = 8080
 
-  Http().bindAndHandle(routes, "localhost", 8080)
-  println(s"Server online at http://localhost:8080/")
+  Http().bindAndHandle(getRoutes, "localhost", port)
 
-  def routes: Route = {
+  println(s"Akka Stream Server running in port $port")
 
-    implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
-    // This is needed for the last `map` method execution
-    implicit val ec: ExecutionContext = system.dispatcher
+  // ADT & JSON FORMAT
+  //####################
 
-    val initialCount = 0
-    val countElement: (Int, DataChunk) => Int = (currentCount: Int, _: DataChunk) => currentCount + 1
+  /**
+    * We create the request data for the POST request,
+    * and also the Json format from Spray library, that we use as implicit conversion to transform a json entry to Data type.
+    */
+
+  case class UserDataType(id: Int, name: String, age: Int, sex: String)
+
+  implicit val requestDataJsonFormat: RootJsonFormat[UserDataType] = jsonFormat4(UserDataType.apply)
+
+  var users = Map[Int, UserDataType]()
+
+  /**
+    * Function where we define all routes of our service.
+    * We are able to use [asSourceOf] function which thanks to implicit [EntityStreamingSupport]
+    * which expect a Data type to be able to marshall to Json we are able to transform into Akka [Source]
+    * We are able to Marshall from Json to Data type [UserDataType] thanks to implicit conversion RootJsonFormat[UserDataType]
+    *
+    * Once we have the Source from our request we can use [runFold] operator to be able to receive the whole json request chunk in
+    * each [UserDataType] entry, to process the whole request as a stream of data.
+    *
+    * @return Route type that it will used by [Http().bindAndHandle] to route request to the proper handler.
+    */
+  private def getRoutes: Route = {
+
+  implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
 
     post {
-      entity(asSourceOf[DataChunk]) { source =>
-        val program: Future[String] = {
+      entity(asSourceOf[UserDataType]) { source =>
+        complete {
           source
-            .log("received").withAttributes(Attributes.logLevels(onElement = Logging.InfoLevel))
-            .runFold(initialCount)(countElement)
-            // map the materialized value
-            .map { count => s"You sent $count chunks" }
+            .runFold(users)((users, user) => {
+              users == users ++ Map(user.id -> user)
+              println(s"New user ${user.name} added")
+              users
+            }).map(users => s"total number of users in system ${users.size}")
         }
-        complete(program)
       }
     }
   }
 }
 
-
-case class DataChunk(id: Int, data: String)
-
-object DataChunk {
-  implicit val dataChunkJsonFormat: RootJsonFormat[DataChunk] = jsonFormat2(DataChunk.apply)
-}
